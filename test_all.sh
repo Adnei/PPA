26e1fef05145318277b621ca0824b56ab1c8ab50
Adnei <adneiwillian@gmail.com>
2018-09-25 21:10:11 -0300

    Adjust

--- test_all.sh
+++ test_all.sh
@@ -5,11 +5,11 @@ echo "Executing tests...";
 echo "#mtx;th;time" > "${FILE}";
 for i in {100,500,1000,1500}; do
 	for th in {1,5,10,15,20,25,30}; do
-    RES_SUM=0;
-    for exec_idx in `seq 1 ${2}`; do
-      RES_SUM=$( awk "BEGIN {print ${RES_SUM}+$( ./${1} ${i} ${th} ) ; exit}" );
-    done;
-    AVG=$( python -c "print ${RES_SUM}/${2}" );
+		RES_SUM=0;
+		for exec_idx in `seq 1 ${2}`; do
+			RES_SUM=$( awk "BEGIN {print ${RES_SUM}+$( ./${1} ${i} ${th} ) ; exit}" );
+		done;
+		AVG=$( python -c "print ${RES_SUM}/${2}" );
 		echo "${i};${th};${AVG}" >> "${FILE}";
 	done;
 done;
